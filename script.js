12be7ebdcd2effff8cd8ae5980ecfb579fff0756
Chris Williams <cjwolliams@gmail.com>
2022-07-31 19:15:55 -0700

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Chris Williams";
-document.querySelector("body").appendChild(h2);
+const h1 = document.createElement("h1");
+h1.textContent = "Chris Williams";
+document.querySelector("body").appendChild(h1);
